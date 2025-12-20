77f9bdf76e67108acf76d3c5a15e5d35851e1c27
Sadia1609 <sadia.rahman160920@gmail.com>
2025-12-20 15:56:15 +0600

    added donar role

---
+++ index.js
@@ -0,0 +1,119 @@
+//initial configuration
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 3000
+
+
+const app = express();
+app.use(cors());
+app.use(express.json())
+//end initial config
+
+
+
+//from mongodb 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://missionscic11:JCs7DKooZkysSMFN@cluster0.e62g5zs.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+
+    //create API
+    const database = client.db('missionscic11DB')
+    const userCollections = database.collection('user')
+    const productCollections = database.collection('product')
+
+    //save in database user info with post
+    app.post('/users', async(req,res)=>{
+        const userInfo = req.body;
+        userInfo.createdAt = new Date();
+        userInfo.role = 'donar'
+        const result = await userCollections.insertOne(userInfo);
+
+        res.send(result)
+
+    })
+
+
+    //create api
+    app.get('/users/role/:email', async(req,res)=>{
+        const {email} = req.params
+        console.log(email);
+        
+
+        const query = {email:email}
+        const result = await userCollections.findOne(query)
+        console.log(result);
+        
+        res.send(result)
+    })
+
+
+    //add products api(sent data frontend to backend)
+    app.post('/products', async(req,res)=>{
+      const data = req.body;
+      data.createdAt = new Date();
+      const result = await productCollections.insertOne(data)
+
+      //sent data frontend to backend
+      res.send(result)
+    })
+
+    //get manager products
+    app.get('/manager/products/:email', async(req,res)=>{
+        const email = req.params.email;
+        const query = {managerEmail: email};
+
+        const result = await productCollections.find(query).toArray();
+        res.send(result)
+    })
+
+
+
+
+
+
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send("Hello, Mission SCIC")
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on ${port}`);
+    
+})
